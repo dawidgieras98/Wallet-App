896ae7e681ec4de7d0194f1ae861fa088ab710c2
Dawid Gieras <97906443+dawidgieras98@users.noreply.github.com>
2022-04-23 21:23:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+const incomeSection = document.querySelector('.income-area')
+const expensesSection = document.querySelector('.expenses-area')
+const availableMoney = document.querySelector('.available-money')
+const addTransactionPanel = document.querySelector('.add-transaction-panel')
+const nameInput = document.querySelector('#name')
+const amountInput = document.querySelector('#amount')
+const categorySelect = document.querySelector('#categoryOne')
+const addTransactionBtn = document.querySelector('.add-transaction')
+const saveBtn = document.querySelector('.save')
+const cancelBtn = document.querySelector('.cancel')
+const deleteBtn = document.querySelector('.delete')
+const deleteAllBtn = document.querySelector('.delete-all')
+const darkTheme = document.querySelector('.dark')
+const lightTheme = document.querySelector('.light')
+
+let root = document.documentElement
+let ID = 0
+let categoryIcon
+let selectedCategory
+let moneyArr = [0]
+
+
+
+const showPanel = () => {
+    addTransactionPanel.style.display = 'flex'
+
+}
+const closePanel = () => {
+    addTransactionPanel.style.display = 'none'
+    clearInputs()
+}
+
+// sprawdzanie czy wszystkie pola są wypełnione
+
+const checkForm = () => {
+    if (nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none'){
+        createNewTransaction()
+    } else {
+        alert('Fill all the fields first')
+    }
+}
+
+
+// reset forma
+const clearInputs = () => {
+
+    nameInput.value = '' 
+    amountInput.value = '' 
+    categorySelect.selectedIndex = 0    
+    
+}
+
+// Nowy task, dodawanie klasy, nadanie atrybutu
+
+const createNewTransaction = () => {
+    const newTransaction = document.createElement('div')
+    newTransaction.classList.add('transaction')
+    newTransaction.setAttribute('id', ID)
+
+    checkCategory(selectedCategory)
+    
+    newTransaction.innerHTML = `
+    <p class="transaction-name">${categoryIcon} ${nameInput.value}</p>
+    <p class="transaction-amount">${amountInput.value}$ <button class="delete" onclick="deleteTransatcion(${ID})"><i class="fas fa-times"></i></button>
+    </p>`
+    
+
+    // nadawanie klasy dla poszczególnych transakcji czy expense czy income
+    amountInput.value > 0 ? incomeSection.appendChild(newTransaction) && newTransaction.classList.add('income') :  expensesSection.appendChild(newTransaction) && newTransaction.classList.add('expense') 
+    moneyArr.push(parseFloat(amountInput.value))
+    countMoney(moneyArr)
+
+    closePanel()
+    ID++
+    clearInputs()
+}
+
+// dołożenie konkretnej ikony  poprzez sprawdzanie wybranej kategorii w popupie
+const checkCategory = transaction => {
+    switch (transaction) {
+        case '[ + ] income':
+            categoryIcon = '<i class="fas fa-money-bill-wave"></i>'
+            break;
+        case '[ - ] Shopping':
+            categoryIcon = '<i class="fas fa-cart-arrow-down"></i>'
+            break;
+        case '[ - ] Food':
+            categoryIcon = '<i class="fas fa-hamburger"></i>'
+            break;
+        case '[ - ] Entertainment':
+            categoryIcon = '<i class="fas fa-film"></i>'
+            break;
+    }    
+}
+
+const selectCategory = () => {
+    selectedCategory = categorySelect.options[categorySelect.selectedIndex].textContent
+    // console.log(selectedCategory);
+}
+
+const countMoney = money => {
+    const newMoney = money.reduce((x, y) => x + y)
+    availableMoney.textContent = `${newMoney}$`
+}
+
+// usuwanie elementu (transakcji) z tablicy
+
+const deleteTransaction = id => {
+     const transactionToDelete = document.getElementById(id)
+     const transactionAmount = parseFloat(transactionToDelete.childNodes[3].innerText)
+     const indexOfTransaction = moneyArr.indexOf(transactionAmount)
+     moneyArr.splice(indexOfTransaction, 1)
+     countMoney(moneyArr)
+
+     transactionToDelete.classList.contains('income') ? incomeSection.removeChild(transactionToDelete) : expensesSection.removeChild(transactionToDelete) 
+}
+
+const deleteAllTransactions = () => {
+    incomeSection.innerHTML = '<h3>Income</h3>'
+    expensesSection.innerHTML = '<h3>Expenses</h3>'
+    availableMoney.textContent = '0$'
+    moneyArr = [0]
+}
+
+const changeStyleToLight = () => {
+    root.style.setProperty('--first-color', '#F9F9F9')
+    root.style.setProperty('--second-color', '#14161F')
+    root.style.setProperty('--border-color', 'rgba(0, 0, 0, .2)')
+}
+
+const changeStyleToDark = () => {
+    root.style.setProperty('--first-color', '#14161F')
+    root.style.setProperty('--second-color', '#F9F9F9')
+    root.style.setProperty('--border-color', 'rgba(255, 255, 255, .4)')
+}
+
+addTransactionBtn.addEventListener('click', showPanel)
+cancelBtn.addEventListener('click', closePanel)
+saveBtn.addEventListener('click', checkForm)
+deleteAllBtn.addEventListener('click', deleteAllTransactions)
+lightTheme.addEventListener('click', changeStyleToLight)
+darkTheme.addEventListener('click', changeStyleToDark)
+
